d918e3235d294a87c15659aa10703734cc5a35e1
Bankole Ebenezer <bankoleebenezer111@gmail.com>
2026-01-13 07:54:53 -0800

    update the main branch

--- App.tsx
+++ App.tsx
@@ -44,7 +44,7 @@ const SPIRITUAL_QUOTES = [
 const ALLOWED_ADMINS = [
   "bankoleebenezer111@gmail.com",      // <--- Put your exact Google email here
   "samidowu2001@gmail.com",    // <--- Put the 2nd admin's email here
-  "third.admin@gmail.com"      // <--- Put the 3rd admin's email here
+  "ife.victor1830@gmail.com"      // <--- Put the 3rd admin's email here
 ];
 
 const App: React.FC = () => {
